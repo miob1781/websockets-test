7480de99547cf399b4df2711bc0062797ddb0aba
Michael Oberst <mioberst@gmx.de>
2022-09-12 10:31:25 +0200

    create basic chat functionality

--- index.js
+++ index.js
@@ -6,16 +6,15 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  })
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg)
+    });
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
